97873b22e01ddfe5ae2d40f92aabb6b38a335498
Uwe Tilemann <uwe.tilemann@snabble.io>
2022-12-10 13:05:50 +0100

    Initial Commit

---
+++ Tests/iOSStringScannerTests/iOSStringScannerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iOSStringScanner
+
+final class iOSStringScannerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iOSStringScanner().text, "Hello, World!")
+    }
+}
